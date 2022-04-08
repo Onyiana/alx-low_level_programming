aa9e6e7b3c6b4888df2ee7bbaa79b9a9ebda5939
root <adaurecananaba@gmail.com>
2022-04-08 12:51:05 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+Â#include <stdio.h>
 #include <unistd.h>
 
 /**
-Â* main-Entry point
+* main-Entry point
 *
 * Return: 0 if successful
 */
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
