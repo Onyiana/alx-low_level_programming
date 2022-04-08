386918379849bcfffbbbb9768148ecdee7019538
root <adaurecananaba@gmail.com>
2022-04-07 18:52:06 -0700

    Hello put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#including <stdio.h>
+/**
+*main - Entry point 
+*
+Return: 0 after printing the function
+*/
+int main(void)
+{
+puts("\"programming is like a multilingual puzzle);
+return (0);
+}
