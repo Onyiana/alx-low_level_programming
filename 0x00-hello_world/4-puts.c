dc36033e51e0c36081a60139542f4fda143d7d77
root <adaurecananaba@gmail.com>
2022-04-08 07:14:25 -0700

    Hello put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* main - Entry point
-* 
-* Rerturn: 0 if successful
+* main - Entry point 
+*
+* Return: 0 if successful
 */
 int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
