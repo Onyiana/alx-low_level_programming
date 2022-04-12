d3d38607e0bf62e053c0e8d09bcb4374f8594130
root <adaurecananaba@gmail.com>
2022-04-12 02:49:18 -0700

    Size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE -o ${CFILE//'.c'/'.s'}
+#include <stdio.h>
+/**
+* main-Eintry point
+*
+* Return: 0 if successful
+*/
+int main(void)
+{
+printf(size of a char: %zu byte(s)\n", sizeof(char));
+printf(size of an int: %zu byte(s)\n", sizeof(int));
+printf(size of a long int: %zu byte(s)\n", sizeof(long int));
+printf(size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf(size of a float: %zu byte(s)\n", sizeof(float));
