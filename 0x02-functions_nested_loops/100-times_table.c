3d794c593c91baac12eb5c4acb33887605a08fe5
root <adaurecananaba@gmail.com>
2022-04-14 18:16:14 -0700

    commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
- /**
+/**
  * print_times_table -  times table
  * @n : times table to use
  * Return:void
  */
- *
- * void print_times_table(int n);
+ * void print_times_table(int n)
  * {
  *
  * int a = 0, rep, b;
