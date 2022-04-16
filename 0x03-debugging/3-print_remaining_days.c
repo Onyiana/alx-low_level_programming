2f55cc61afc76d1d0ec7c15f33db92246dd68498
root <adaurecananaba@gmail.com>
2022-04-16 15:40:17 -0700

    debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -3,8 +3,8 @@
 
 /**
 * print_remaining_days - takes a date and print how many days are 
-* left in the year, taking leap year into account
-* @month: month in the number format
+* left in the year, taking leap years into account
+* @month: month in number format
 * @day: day of month
 * @year: year
 * Return: void
@@ -19,7 +19,7 @@ if (month > 2 && day >= 60)
 day++;
 }
 printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 365 - day);
+printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
