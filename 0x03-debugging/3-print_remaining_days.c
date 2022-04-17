a3938b54f727a19a668dc48512cabbe767bb8591
root <adaurecananaba@gmail.com>
2022-04-17 08:38:24 -0700

    debug

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -18,9 +18,9 @@ if (month > 2 && day >= 60)
 {
 day++;
 }
-printf("Day of the year: %d\n", day);
+ printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
-}
+  }
 else
 {
 if (month == 2 && day == 60)
