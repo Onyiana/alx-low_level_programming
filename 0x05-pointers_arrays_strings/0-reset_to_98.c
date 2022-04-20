82a692968765385c2017b948fae75a91841047be
root <adaurecananaba@gmail.com>
2022-04-20 14:33:27 -0700

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,13 @@
 
 /**
  * reset_to_98-update the value *n points to.
- *@n: the number to modify.
+ * @n: the number to modify.
+ * Return: void
  */
 
 void reset_to_98(int *n);
 {
+
 *n = 98;
+
 }
