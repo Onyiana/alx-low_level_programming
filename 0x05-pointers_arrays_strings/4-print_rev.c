4a0f7dd0ac69890cc1bcf0143be2cf8873eb8319
root <adaurecananaba@gmail.com>
2022-04-20 21:00:02 -0700

    Rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,7 +6,7 @@
  * Rerurn: void
  */
 
-voide print_rev(char *s)
+void print_rev(char *s)
 {
 int i;
 
@@ -18,8 +18,8 @@ i++;
 
 for (i = i - 1; i >= 0; i--)
 {
-_putchar (s[i]);
+_putchar(s[i]);
 }
 
-_putchar ('\n');
+_putchar('\n');
 }
