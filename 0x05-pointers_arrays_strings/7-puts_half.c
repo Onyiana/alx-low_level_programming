c2a0396d79c80295ec1e5bcc7a888017d6bb7293
root <adaurecananaba@gmail.com>
2022-04-20 22:10:11 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half-print every other character of a string
+ * @str: a pointer to an int that will be changed
+ *
+ * Return: void
+ */
+
+void putd_half(char *str)
+
+{
+int i, last;
+
+i = 0;
+while (str[i] != '\0')
+{
+i++;
+}
+
+last = (i + 1) / 2;
+
+for (i = last; str[i]; i++)
+{
+_putchar (str[i]);
+}
+
+_putchar ('\n');
+
+}
