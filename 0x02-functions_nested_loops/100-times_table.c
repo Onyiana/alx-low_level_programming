aa51f43747dfda93e163c4f81512be6d0a1f7516
root <adaurecananaba@gmail.com>
2022-04-22 02:06:13 -0700

    table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,13 +3,18 @@
 /**
  * print_times_table -  times table
  * @n : times table to use
+ * Description: print times table
  * Return:void
  */
+
 void print_times_table(int n) 
 {
+
 int a = 0, rep, b;
+
 if (n < 0 || n >15)
 return;
+
 while(a <= n)
 {
 for (b = 0; b <= n; b++)
