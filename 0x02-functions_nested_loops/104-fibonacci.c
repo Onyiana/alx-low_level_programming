f082fec51ec069e853a8339a8fd1d58d8359ecfa
root <adaurecananaba@gmail.com>
2022-04-22 03:44:26 -0700

    fibo

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,13 +4,14 @@
  * main-find and prints the first 98 fibonacci numbers
  * starting with 1 and 2
  * followed by a new line
+ * Description: print the first 98 fibonacci numbers
  * Return: 0 if successful
  */
 int main(void)
 {
 unsigned long int i, j, k, j1, j2, k1, k2;
 
-j =1;
+j = 1;
 k = 2;
 
 printf("%lu", j);
