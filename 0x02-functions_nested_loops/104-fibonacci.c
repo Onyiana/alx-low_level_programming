a44c3fdf914045140024bdc2dd2b3a213b42457e
root <adaurecananaba@gmail.com>
2022-04-22 03:39:07 -0700

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,7 +6,6 @@
  * followed by a new line
  * Return: 0 if successful
  */
-
 int main(void)
 {
 unsigned long int i, j, k, j1, j2, k1, k2;
@@ -22,11 +21,13 @@ printf(", %lu", k);
 k = k + j;
 j = k - j;
 }
+
 j1 = j / 1000000000;
 j2 = j % 1000000000;
 k1 = k / 1000000000;
 k2 = k % 1000000000;
-for (i = 92; i < 98; ++i)
+
+for (i = 92; i < 99; ++i)
 {
 printf(", %lu", k1 + (k2 / 1000000000));
 printf("%lu", k2 % 100000000);
@@ -35,6 +36,7 @@ j1 = k1 - j1;
 k2 = k2 + j2;
 j2 = k2 - j2;
 }
+
 printf("\n");
 
 return (0);
