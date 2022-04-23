73f5139fb985329346f29ec9093bd6610d6d104f
root <adaurecananaba@gmail.com>
2022-04-23 05:49:05 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * *_strcat - concatenate two strings
  * @dest: pointer destination
@@ -8,16 +9,19 @@
 
 char *_strcat(char *dest, char *src)
 {
-int a = -1, i;
-for (i = 0; dest[i] != '\0'; i++);
+int i = 0, j = 0;
 
-do
-{
-a++;
-dest[i] = src[a];
+while (dest[i] != '\0')
 i++;
+
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
 }
-while (src[a] != '\0');
+
+dest[i] = '\0';
 
 return (dest);
 }
