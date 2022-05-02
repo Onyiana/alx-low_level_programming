87c538bf8d48b4293cd95993b989407c02e3231f
onyiana <adaurecananaba@gmail.com>
2022-05-02 15:21:52 -0700

    commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * main - Entry point
  * @argc: count the number of parameters that goes into the main.
  * @argv: pointer of arrays of pointers that contains strings entering main.
- * Return: 1 if successful.
+ * Return: 0
  */
 
 int main(int argc, char **argv)
