221220218eccc4a49a0ff25df3cf8443243fb139
onyiana <adaurecananaba@gmail.com>
2022-05-02 15:29:53 -0700

    args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,10 +5,10 @@
  * main - Entry point
  * @argc: count the number of parameters that goes into the main.
  * @argv: pointer of arrays of pointers that contains strings entering main.
- * Return: 0
+ * Return: void
  */
 
 int main(int argc, char **argv)
 {
 printf("Number of argument: %d\n", argc - 1);
-}[A[A[A[A[A[A[C[C[C[C[C[C[C[C[C[C[void[B[B[B[B[B[B[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D
+}
